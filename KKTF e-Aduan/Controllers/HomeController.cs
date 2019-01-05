9a8297bfb54af61b387d6b05aec3cf122119bfe8
NorFadila Saidi <norfadilasaidi@gmail.com>
2019-01-06 01:54:42 +0800

    Add

--- KKTF e-Aduan/Controllers/HomeController.cs
+++ KKTF e-Aduan/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace KKTF_e_Aduan.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();//
         }
 
         public ActionResult About()
