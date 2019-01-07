fe0af894d932f108b7e8f6276d29b5895e3aaea8
Silmi Fattah <silmifattah.sf@gmail.com>
2019-01-07 21:42:42 +0800

    Add Technician

---
+++ KKTF e-Aduan/Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KKTF_e_Aduan.Models;
+
+namespace KKTF_e_Aduan.Controllers
+{
+    public class TestController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            MyDatabaseEntities db = new MyDatabaseEntities();
+
+           return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SaveRecord(EmployeeViewModel model)
+        {
+            
+            try
+            {
+                MyDatabaseEntities db = new MyDatabaseEntities();
+
+                technician tech = new technician();
+                tech.techId = model.TechID;
+                tech.techName = model.Name;
+                tech.techAddress = model.Address;
+                tech.techPhoneNo = model.PhoneNo;
+                db.technicians.Add(tech);
+                db.SaveChanges();
+                int latesttechId = tech.techId;
+                return RedirectToAction("Index");
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+            
+        }
+    }
+}
+        
---
+++ KKTF e-Aduan/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KKTF_e_Aduan.Models
+{
+    public class EmployeeViewModel
+    {
+        public int TechID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
